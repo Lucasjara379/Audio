64045b0ab572618f7c456e9a2a1d16d8c6b41741
Lucas Jarana <lucas.jarana24@imperial.ac.uk>
2026-05-14 12:38:35 +0100

    fix the whole audio_clips shabang

--- main.py
+++ main.py
@@ -3,19 +3,32 @@ import librosa
 import numpy as np
 import sys
 
-if not Path("audio_clips").exists():
+
+p = Path("audio_clips")
+if not p.is_dir():
     print("No folder called audio_clips exists in working directory.")
-    sys.exit()
+    sys.exit(1)
 
 # get the audio files loaded into script
-filename1 = input("Input first audio file: ")
-filename2 = input("Input second audio file: ")
-audio1, audio2 = Path("audio_clips") / filename1
+filename1 = input("Input first audio file that's inside audio_clips: ")
+audio1 = p / filename1
+if not audio1.is_file():
+    raise FileNotFoundError(
+        "No audio clip exists with filename that was inputted."
+    )
+
+filename2 = input("Input second audio file that's also in audio_clips: ")
+audio2 = p / filename2
+if not (p / filename1).is_file():
+    raise FileNotFoundError(
+        "No audio clip exists with filename that was inputted."
+    )
+
 y1, sr1 = librosa.load(audio1)
 y2, sr2 = librosa.load(audio2)
 
 # convert to mfcc which is apparently good for noticing similarities between songs
-mfcc1 = librosa.feature.mfcc(y1, sr1, n_mfcc=13)
+mfcc1 = librosa.feature.mfcc(y=y1, sr=sr1, n_mfcc=13)
 mfcc2 = librosa.feature.mfcc(y2, sr2, n_mfcc=13)
 
 # Use the dynamic time warping algorithm for similarity
@@ -29,3 +42,5 @@ M = np.shape(mfcc2)[1]
 # Normalise and evaluate similarity constant between 0 and 1
 normalised_distance = raw_distance / (N + M)
 similarity = 1 / (1 + normalised_distance)
+
+print(f"The similarity of the two audio clips has a value of:\n{similarity}!")
